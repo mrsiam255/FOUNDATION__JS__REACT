47b359fc239508cf84d4ce3e49e402db78f8e218
mrsiam255 <ferdoussam39@gmail.com>
2026-08-23 02:20:59 +0600

    Initial commit

---
+++ var.js
@@ -0,0 +1,14 @@
+let a=5;
+a=9;
+console.log(a);
+
+// answer will be 9 cz let diya last r value ta output dai 
+
+// aa="siam";
+const aa="prity";
+
+console.log(aa);
+const siam=true;
+
+
+console.log(typeof (aa),typeof(a),typeof(siam));
