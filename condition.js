8d638fef1d5cd5f9ba3788301744677f84fc0803
mrsiam255 <ferdoussam39@gmail.com>
2026-08-23 16:04:00 +0600

    switcch statement

---
+++ condition.js
@@ -0,0 +1,13 @@
+let num=33;
+
+switch (true) {
+    case  num>=80:
+    console.log("A+");
+    case num>=60:
+    console.log("Pass");
+        break;
+
+    default:
+        console.log("improvement ");
+        break;
+}
