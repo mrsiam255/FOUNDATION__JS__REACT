f25301f0819e89178dfe6cf8c1e720ffa32d017b
mrsiam255 <ferdoussam39@gmail.com>
2026-08-23 13:43:39 +0600

    operatore

---
+++ operator.js
@@ -0,0 +1,23 @@
+//  
+
+const a=10;
+const b=7;
+
+if(a>b){
+    console.log("a is greater ");
+}
+
+
+const c=a>b;
+console.log(c);
+
+const d=(a>b) && !(b>a);
+console.log(d);
+
+const e=(a/b);
+console.log(typeof(e));
+console.log(typeof(a));
+
+const f=(a/b);
+console.log(f.toFixed(2));
+console.log(typeof(f));
