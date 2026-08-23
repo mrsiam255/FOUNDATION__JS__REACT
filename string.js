ae988e73a8ffb81f7e8a7293ae01bb981c861db7
mrsiam255 <ferdoussam39@gmail.com>
2026-08-23 15:49:34 +0600

    string

---
+++ string.js
@@ -0,0 +1,18 @@
+let firstName="Ferdoes";
+let lastName="Siam";
+
+let fullName=firstName +" "+ lastName ;
+console.log(fullName);
+
+
+
+let passWord="12345";
+let oldPass="67";
+let fullpass=passWord + oldPass;
+
+console.log(String(fullpass).length);
+
+let name="siam";
+let address="pabna";
+
+console.log(`name is ${name} and address is : ${address}`,` and city is rajshahi `);
