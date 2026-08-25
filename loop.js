976b3e09f0fec77c23de292cb02cf3e88e660c0f
mrsiam255 <ferdoussam39@gmail.com>
2026-08-25 15:21:19 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,29 @@
+// loop 
+/*
+for(i=0;i<5;i++){
+    console.log("count :", i);
+}
+
+for(i=0;i<5;i++){
+    console.log(`count:${i}`);
+}
+
+    */
+
+
+let fastFood=["burger","pizza","chowmein","noodles","steak"];
+
+for(i=0;i<fastFood.length;i++){
+    console.log("yummy :",fastFood[i]);
+}
+console.log("while r kaj hoba \n\n")
+
+// while loop use 
+
+let fruit=["apple","banana","grape","pine"];
+
+let j=0;
+while(j<fruit.length){
+    console.log("yummy : ",fruit[j]);
+    j++;
+}
