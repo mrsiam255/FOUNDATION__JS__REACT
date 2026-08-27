8c7b6020c5c02846a6226abd3054be539738b081
mrsiam255 <ferdoussam39@gmail.com>
2026-08-27 19:09:11 +0600

    rest obj array

---
+++ restarr.js
@@ -0,0 +1,18 @@
+let pupils=[
+    {
+    name:"siam",
+    age:26,
+    add:"pabna",
+    },
+
+    {
+
+    name2:"prity",
+    age2:27,
+    add:"cyprus",
+    }
+];
+
+const[name,...rest]=pupils;
+console.log(pupils);
+console.log(rest);
