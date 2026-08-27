a58be437ab38e13fc0e09a4fdee8d1866fb52924
mrsiam255 <ferdoussam39@gmail.com>
2026-08-27 14:30:49 +0600

    destructing array

---
+++ arraydetuc1.js
@@ -0,0 +1,17 @@
+/*
+const numbers = [10, 20, 30, 40, 50];
+
+findout ::
+
+10 → first
+30 → third
+50 → fifth
+*/
+
+let arr=[10,20,30,40];
+
+//let [fst:first, ,tst:third]=arr; // aivabe array te hoinai , ata only object a hoi
+
+let [first, , third]=arr;
+
+console.log(first,third);
