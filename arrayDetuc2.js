eeecc2f8d3dabeb6b52cc5b34ff6af3d30436af0
mrsiam255 <ferdoussam39@gmail.com>
2026-08-27 14:33:10 +0600

    destructing array2

---
+++ arrayDetuc2.js
@@ -0,0 +1,14 @@
+/*
+const fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+বের করো:
+
+"Apple" → firstFruit
+"Mango" → thirdFruit
+"Grapes" → lastFruit
+*/
+
+let fruit=["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+let[firstFruit,thirdFruit,lastFruit]=fruit;
+console.log(firstFruit,thirdFruit,lastFruit);
