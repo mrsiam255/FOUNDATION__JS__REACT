cacdc00c7ad8c2aff1ab919d62e1287e09cf05d6
mrsiam255 <ferdoussam39@gmail.com>
2026-08-27 13:47:58 +0600

    destructing

---
+++ back.js
@@ -0,0 +1,13 @@
+// destructing 
+
+let employee ={
+    name : "siam",
+    age : 22,
+    department : "SQA",
+    salary : 50000,
+
+};
+
+let {name , department , salary}= employee ;
+
+console.log(name,department,salary);
---
+++ backlitFunction.js
@@ -0,0 +1,6 @@
+function pay(price){
+    return price*.1;
+}
+
+price=1000;
+console.log(`your discount amount is $${pay(price)} and you have to be paid`);
